de633e60737bc05e1bf20cf7d1529038c6feb311
Hadid Hardiansyah Saputra <hadidhardiansyah@gmail.com>
2024-11-03 22:39:39 +0700

    Initial Commit

---
+++ src/main/java/com/bills/food_ordering/BillsFoodOrderingApplication.java
@@ -0,0 +1,13 @@
+package com.bills.food_ordering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillsFoodOrderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillsFoodOrderingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bills/food_ordering/BillsFoodOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bills.food_ordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillsFoodOrderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
